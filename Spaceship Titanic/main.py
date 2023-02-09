af7df5fe18333378fe64689a9968fe5d4384b6c3
weadione <36260010+weadione@users.noreply.github.com>
2023-02-09 20:39:59 +0900

    Init. Spaceship Titanic competition
     - Init project

---
+++ Spaceship Titanic/main.py
@@ -0,0 +1,17 @@
+import argparse
+import sklearn
+
+def init_parser():
+    parser = argparse.ArgumentParser(description="Method for Spaceship Titanic Kaggle competition")
+    
+    parser.add_argument("--")
+    
+    return parser
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
